6e8a86a20cba7d79d06a241e9553b99a40994846
Infant Ajay Venus <ajay.v@incubyte.co>
2026-03-05 17:32:20 +0530

    chore: update depenedecies and clean up code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,3 @@
-use std::io::stdout;
-
-use ferris_says::say;
-
 fn main() {
-    let stdout = stdout();
-    let message = "Hello from Fellow Rustaceans";
-    let width = message.len();
-    say(message, width, stdout).unwrap();
+    println!("Hello World");
 }
