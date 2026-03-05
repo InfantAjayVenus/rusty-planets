613351585358f9e0b8dec0e4b6fca5d836ee1c80
Infant Ajay Venus <ajay.v@incubyte.co>
2026-03-05 18:34:13 +0530

    feat: add basic window render

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello World");
+use macroquad::prelude::*;
+
+#[macroquad::main("Rusty Planets")]
+async fn main() {
+   loop {
+       clear_background(BLACK);
+       next_frame().await;
+   } 
 }
