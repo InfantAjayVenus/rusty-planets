47296e650614a2d871740b026fada389d22eecd8
Infant Ajay Venus <ajay.v@incubyte.co>
2026-03-05 18:42:43 +0530

    fix: remove offset by radius for center

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,15 @@ use macroquad::prelude::*;
 
 #[macroquad::main("Rusty Planets")]
 async fn main() {
-   loop {
-       clear_background(BLACK);
+    loop {
+        clear_background(BLACK);
 
-       let sun_radius = 30.0;
-       let cx = (screen_width() / 2.0) - sun_radius;
-       let cy = (screen_height() / 2.0) - sun_radius;
+        let sun_radius = 30.0;
+        let cx = screen_width() / 2.0;
+        let cy = screen_height() / 2.0;
 
-       draw_circle(cx, cy, sun_radius, YELLOW);
+        draw_circle(cx, cy, sun_radius, YELLOW);
 
-       next_frame().await;
-   } 
+        next_frame().await;
+    }
 }
