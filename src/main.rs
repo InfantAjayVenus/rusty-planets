f57f25c51b7b2b18e7764e086e03aab5a640635b
Infant Ajay Venus <ajay.v@incubyte.co>
2026-03-07 04:03:31 +0530

    feat: add more planets

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@ use planet::gravity;
 
 #[macroquad::main("Rusty Planets")]
 async fn main() {
-    let sun = Planet::new(vec2(0.0, 0.0), vec2(0.0, 0.0), 255.0, YELLOW);
+    let sun = Planet::new(vec2(0.0, 0.0), vec2(0.0, -7.7), 255.0, YELLOW);
     let earth = Planet::new(vec2(150.0, 0.0), vec2(0.0, 30.0), 10.0, BLUE);
+    let mars = Planet::new(vec2(230.0,0.0), vec2(0.0,1.5), 6.0, RED);
+    let jupiter = Planet::new(vec2(350.0,0.0), vec2(0.0,19.1), 80.0, ORANGE);
 
-    let mut bodies = vec![sun, earth];
+    let mut bodies = vec![sun, earth, mars, jupiter];
 
     loop {
         clear_background(BLACK);
