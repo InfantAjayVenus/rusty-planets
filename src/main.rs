d22650d4d55e69a1b22db2c5ef0c569737dd6d4f
Infant Ajay Venus <ajay.v@incubyte.co>
2026-03-11 05:45:49 +0530

    feat: add a speed control

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use planet::gravity;
 
 #[macroquad::main("Rusty Planets")]
 async fn main() {
+    let mut speed: u32 = 1;
     let mut sun = Planet::new("Sun", vec2(0.0, 0.0), vec2(0.0, 0.0), 255.0, YELLOW);
     sun.set_fixed();
     let earth = Planet::new("Earth", vec2(150.0, 0.0), vec2(0.0, 30.0), 10.0, BLUE);
@@ -14,23 +15,36 @@ async fn main() {
     let mut bodies = vec![sun, earth, mars, jupiter];
 
     loop {
+        if is_key_pressed(KeyCode::K) && speed <= 16 {
+            speed *= 2;
+        }
+
+        if is_key_pressed(KeyCode::J) && speed >= 2 {
+            speed /= 2;
+        }
+
         clear_background(BLACK);
 
-        let forces: Vec<Vec2> = (0..bodies.len())
-            .map(|i| {
-                let mut total = Vec2::ZERO;
-                for j in 0..bodies.len() {
-                    if i != j {
-                        total += gravity(&bodies[i], &bodies[j]);
+        for _ in 0..speed {
+            let forces: Vec<Vec2> = (0..bodies.len())
+                .map(|i| {
+                    let mut total = Vec2::ZERO;
+                    for j in 0..bodies.len() {
+                        if i != j {
+                            total += gravity(&bodies[i], &bodies[j]);
+                        }
                     }
-                }
-                total
-            })
-            .collect();
+                    total
+                })
+                .collect();
 
-        for (body, force) in bodies.iter_mut().zip(forces.iter()) {
-            body.update_velocity(force);
-            body.update_position();
+            for (body, force) in bodies.iter_mut().zip(forces.iter()) {
+                body.update_velocity(force);
+                body.update_position();
+            }
+        }
+
+        for body in bodies.iter() {
             body.draw();
         }
 
